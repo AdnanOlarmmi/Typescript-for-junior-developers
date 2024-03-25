bb0bc33b5941147bc4beeab746974de9cbf2f25e
adnanolarmmi <olarmmi@gmail.com>
2024-03-25 16:45:03 +0100

    Add tsconfig

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let age = 30;
+let firstName = "Maro";
+let isFictional;
+console.log(age);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let age: number = 30;
+let firstName: string = "Maro";
+let isFictional: boolean;
+console.log(age);
